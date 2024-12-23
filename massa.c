c3c0dd6aabb4bdeb025614cb0c89018499d9ba8f
Andre Lopes <allss253@aluno.ufrb.edu.br>
2024-12-23 12:12:39 -0300

    massa por andre

---
+++ massa.c
@@ -0,0 +1,20 @@
+#include "massa.h"
+
+float toneladaPkilo(float medida){
+    return medida*1000;
+}
+float toneladaPgrama(float medida){
+    return medida*1000000;
+}
+float kiloPtonelada(float medida){
+    return medida/1000;
+}
+float kiloPgrama(float medida){
+    return medida*1000;
+}
+float gramaPtonelada(float medida){
+    return medida/1000000;
+}
+float gramaPkilo(float medida){
+    return medida/1000;
+}
