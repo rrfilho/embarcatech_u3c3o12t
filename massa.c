b3b7f76c53f0484f94e1b5d456294e7b228db6e8
André Luís <allss253@aluno.ufrb.edu.br>
2024-12-23 23:48:58 -0300

    Add files via upload

---
+++ massa.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "massa.h"
+
+float toneladaPkilo(float medida){
+    return medida*1000;
+}
+float toneladaPgrama(float medida){
+    return medida*1000000;
+}
+float kiloPtonelada(float medida){
+    return medida/1000;
+}
+float kiloPgrama(float medida){
+    return medida*1000;
+}
+float gramaPtonelada(float medida){
+    return medida/1000000;
+}
+float gramaPkilo(float medida){
+    return medida/1000;
+}
