b3ec8086644a2ea7a795262d21ed3cecb9eafa8d
Jonatas <jonatasdev97@gmail.com>
2024-12-25 19:41:33 -0300

    Codigos de Conversão de Area

---
+++ Conversor_Area.h
@@ -0,0 +1,8 @@
+#ifndef AREA_H
+#define AREA_H
+
+// Funções para conversão de área
+double metros_quadrados_para_centimetros_quadrados(double metros);
+double centimetros_quadrados_para_metros_quadrados(double centimetros);
+
+#endif // AREA_H
